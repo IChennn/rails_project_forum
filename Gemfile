31f2ac124e57a07e93e79c5a4a7133f21e2df422
IChennn <lourpesro@gmail.com>
2017-06-21 19:23:24 +0900

    paginate

--- Gemfile
+++ Gemfile
@@ -28,6 +28,8 @@ gem "devise"
 
 gem "simple_form"
 
+gem "will_paginate"
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,5 @@ class Post < ActiveRecord::Base
 	belongs_to :user
   	belongs_to :group
   	
+  	scope :recent, -> {order("created_at DESC")}
 end
